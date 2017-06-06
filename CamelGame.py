d52f6b2f868109431c4b17127bc28461bc013599
Stephen Erlenbush <stephen.erlenbush@gmail.com>
2017-06-06 08:58:38 -0700

    Finished Product
    Added win scenarios and cleaned up.

--- CamelGame.py
+++ CamelGame.py
@@ -29,36 +29,35 @@ while not done:
     print("D. Stop for the night.")
     print("E. Status check.")
     print("Q. Quit.")
-    if thirst >= 4 and thirst < 6:
-        print("You are getting thirsty!")
-    if thirst == 6:
-        print("You died of thirst!")
-        print("Game over")
-        done = True
     user_choice = input("What is your choice? ")
+
 # User chose to quit loop
     if user_choice.upper() == "Q":
         done = True
+
 # User chose status check
     elif user_choice.upper() == "E":
         print("Miles Traveled: ", miles_traveled)
         print("Drinks available: ", drinks_in_canteen)
         print("The natives are ", miles_traveled - native_travel_distance, " miles behind you!")
+
 # User chose to stop for the night
     elif user_choice.upper() == "D":
         camel_exhaustion = 0
         print("The camel praises you for your wonderful choice!")
-        native_travel = random.randint(7,14)
-        native_travel_distance = native_travel_distance + native_travel
+        native_travel = random.randint(7, 14)
+        native_travel_distance += native_travel
+
 # User chose to go full speed ahead!
     elif user_choice.upper() == "C":
         travel = random.randint(10, 20)
         miles_traveled = miles_traveled + travel
         print("You traveled ", travel, " more miles!")
         thirst += 1
-        camel_exhaustion = camel_exhaustion + random.randint(1,3)
+        camel_exhaustion = camel_exhaustion + random.randint(1, 3)
         native_travel = random.randint(7, 14)
         native_travel_distance = native_travel_distance + native_travel
+
 # User chose the moderate speed option
     elif user_choice.upper() == "B":
         travel = random.randint(5, 12)
@@ -68,6 +67,7 @@ while not done:
         camel_exhaustion += 1
         native_travel = random.randint(7, 14)
         native_travel_distance = native_travel_distance + native_travel
+
 # User has chosen to take a drink from their canteen
     elif user_choice.upper() == "A":
         if drinks_in_canteen >= 1:
@@ -76,3 +76,42 @@ while not done:
             print("You took a drink from your canteen! There are: ", drinks_in_canteen, " drinks left!")
         else:
             print("You have no drinks available!")
+
+
+# Create fail reasons for thirst
+    if 4 <= thirst < 6:
+        print("You are getting thirsty!")
+    elif thirst == 6:
+        print("You died of thirst!")
+        print("Game over.")
+        done = True
+
+# Create fail conditions for camel
+    if 5 < camel_exhaustion <= 8:
+        print("Your camel is getting tired. You should rest.")
+    elif camel_exhaustion > 8:
+        print("Your camel is dead.")
+        print("Game over.")
+        done = True
+
+# Create fail condition for natives catching up
+    if miles_traveled <= native_travel_distance >= miles_traveled:
+        print("The natives have caught you.")
+        print("Game over.")
+        done = True
+    elif miles_traveled - native_travel_distance < 15:
+        print("The natives are getting close!")
+
+# Create win scenario
+    if miles_traveled >= 200 and done == False:
+        print("Congratulation!!!")
+        print("You beat the game!")
+        done = True
+
+# Random Oasis scenario
+    if random.randint(0, 20) == 20:
+        print("You found a secret oasis!")
+        print("Your canteen, thirst, and camel exhaustion has been reset!")
+        thirst = 0
+        camel_exhaustion = 0
+        drinks_in_canteen = 5
